fa27997d29000d1e13c2d509c7b01514544eb1d0
Rostyk <kremen.rostylsav@gmail.com>
2026-03-03 12:50:49 +0100

    FaceId funktion

---
+++ FaceId.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('img/people_2.jpeg')
+gray= cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+faces = cv2.CascadeClassifier('faces.xml')
+
+results = faces.detectMultiScale(gray, scaleFactor=2, minNeighbors=3)
+
+
+for (x, y, w, h) in results:
+    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), thickness=2)
+
+cv2.imshow("Result", img)
+cv2.waitKey(0)
